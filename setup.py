26c3d4c7e32405367b7d5c939d1358dda3ee1d94
Raul Andrade <raul.sandrade@hotmail.com>
2025-12-15 20:47:27 -0300

    set up project initial organization and metadata. add setup.py, main, initfor package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "dundie",
+    version = "0.1.0",
+    description= "Reward system for Dunder Mifflin",
+    author="Raul Andrade",
+    packages=find_packages(),
+
+)
