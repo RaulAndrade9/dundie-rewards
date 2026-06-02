0b76193c2006188755cec7d6927bd018bf47032a
Raul Andrade <raul.sandrade@hotmail.com>
2026-06-02 20:01:42 -0300

    requicls

--- setup.py
+++ setup.py
@@ -1,19 +1,44 @@
 from setuptools import setup, find_packages
+import os
+
+def read(*paths):
+    """Read the contents of a text file safely.
+    >>>read("dundie", "VERSION")
+    '0.1.0'
+    >>>read("README.md)
+    ...
+    """
+
+    roothpath = os.path.dirname(__file__)
+    filepath = os.path.join(roothpath, *paths)
+    with open(filepath, encoding="utf-8")as file_:
+        return file_.read().strip()
+    
+
+def read_requirements(path):
+    """Return a list of requirements for a text file"""
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(("#", "git+", '"', "-"))
+    ]
 
 setup(
     name = "dundie",
     version = "0.1.0",
     description = "Rewards points system for Dunder Mifflin",
+    long_description= read("README.md"),
+    long_description_content_type= "text/markdown",
     author = "Raul Andrade",
     packages = find_packages(),
-    name="dundie",
-    version="0.1.0",
-    description="Rewards points system for Dunder Mifflin",
-    author="Raul Andrade",
-    packages=find_packages(),
     entry_points={
         "console_scripts": [
             "dundie=dundie.__main__:main"
         ]
+    },
+    install_requires = read_requirements("requirements.txt"),
+    extra_require ={
+        "test": read_requirements("requirements.test.txt"),
+        "dev" : read_requirements("requirements.dev.txt"),
     }
 )
