3c73148d226dd3484b3698abf2f0d26518d305de
Raul Andrade <raul.sandrade@hotmail.com>
2026-06-01 22:21:51 -0300

    iniciando issue 11

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,3 +1,43 @@
 
+import argparse
+
+def load(filepath):
+    """Loads datas from filepath to database"""
+    try:
+        with open(filepath)as file:
+            for line in file:
+                print(line)
+    except FileNotFoundError as e:
+        print(f"File not found {e}")
+
+
+
+
 def main():
     print("Executing Dundie from entry point")
+
+    parser = argparse.ArgumentParser(
+    description = "Dunder Mifflin Rewards CLI",
+    epilog = "Enjoy and use with cautions"
+)
+    parser.add_argument(
+        "subcommand",
+        type = str,
+        help = "The subcomand to run",
+        choices = ("load", "show", "send"),
+    )
+
+    parser.add_argument(
+        "filepath",
+        type = str,
+        help = "Filepath to load",
+    )
+
+    args = parser.parse_args()
+    
+    globals()[args.subcommand](args.filepath)
+    
+    
+
+if __name__ == "__main__":
+    main()
